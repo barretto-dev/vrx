59c3cc526e14da2b6ac113afd567d60098cdd5b6
barretto-dev <barretto.dev@gmail.com>
2026-03-06 16:58:44 -0300

    Add main.cpp on wildlife package

---
+++ wildlife/src/main.cpp
@@ -0,0 +1,44 @@
+#include <rclcpp/rclcpp.hpp>
+#include <chrono>
+#include <thread>
+#include <atomic>
+#include <csignal>
+#include <iostream>
+
+#include "wildlife/wildlife_node.hpp"
+
+using namespace std::chrono_literals;
+
+static std::atomic_bool g_stop_requested{false};
+static void sigint_handler(int) { g_stop_requested.store(true); }
+
+int main(int argc, char **argv)
+{
+  rclcpp::InitOptions opts;
+  opts.shutdown_on_signal = false;
+  rclcpp::init(argc, argv, opts);
+
+  std::signal(SIGINT, sigint_handler);
+
+  auto node = std::make_shared<Wildlife>();
+  rclcpp::executors::SingleThreadedExecutor exec;
+  exec.add_node(node);
+
+  while (rclcpp::ok() && !g_stop_requested.load()) {
+    exec.spin_some();
+    std::this_thread::sleep_for(10ms);
+  }
+
+  // node->cancel_timer();
+  // std::cerr << "[wildlife] Ctrl+C received. Sending STOP...\n";
+
+  // for (int i = 0; i < 50; ++i) {
+  //   node->stop_vehicle();
+  //   exec.spin_some();
+  //   std::this_thread::sleep_for(20ms);
+  // }
+
+  exec.remove_node(node);
+  rclcpp::shutdown();
+  return 0;
+}
